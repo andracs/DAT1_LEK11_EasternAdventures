457cae23d2a0dd78d2ecb9aeb0488aa1dc06af98
Erik Ruhmanis <erik2310@edu.easj.dk>
2017-11-02 14:48:12 +0100

    Opdateret Alien og LaserPistol klassen med look() metoderne.

--- src/dk/hungarian/genstande/LaserPistol.java
+++ src/dk/hungarian/genstande/LaserPistol.java
@@ -2,10 +2,16 @@ package dk.hungarian.genstande;
 
 import dk.hungarian.Genstand;
 
+// Klassen er lavet af erik2310
+
 public class LaserPistol extends Genstand {
 
     public LaserPistol() {
         super("Laser Pistol", "En laser pistol som aliens kan skyde med.", 50);
     }
 
+
+    public String look() {
+        return "Du ser en Laser Pistol.";
+    }
 }
--- src/dk/hungarian/skabninger/Alien.java
+++ src/dk/hungarian/skabninger/Alien.java
@@ -3,6 +3,8 @@ package dk.hungarian.skabninger;
 import dk.hungarian.Gender;
 import dk.hungarian.Skabning;
 
+// Klassen er lavet af erik2310
+
 public class Alien extends Skabning {
 
     int alder;
@@ -17,5 +19,10 @@ public class Alien extends Skabning {
     @Override
     public String toString() {
         return "Aliens navn er " + this.getNavn() + ", den er " + this.alder + ", det er en " + this.getGender() + " og han kommer fra " + this.oprindelsesPlanet;
+    }
+
+
+    public String look() {
+        return "Du ser en Alien.";
     }
 }
