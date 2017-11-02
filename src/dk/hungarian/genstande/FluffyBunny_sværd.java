62dd30030e8cdccdd774b2f03216ef3410be432d
Johan <Joha6163@edu.easj.dk>
2017-11-02 14:59:46 +0100

    lille rettelse

--- src/dk/hungarian/genstande/FluffyBunny_sværd.java
+++ src/dk/hungarian/genstande/FluffyBunny_sværd.java
@@ -3,6 +3,7 @@ package dk.hungarian.genstande;
 import dk.hungarian.Genstand;
 
 public class FluffyBunny_sværd extends Genstand {
+    //Johans arbejde
     //Tilføjer en lyd til sværdet.
     String lyd = "Ffffkrrrrshhzzzwooooom..woom..woooom..";
 
@@ -16,6 +17,14 @@ public class FluffyBunny_sværd extends Genstand {
         //Navn, beskrivelse og styrke.
         super("FluffyBunny", "Legendarisk sværd", 100);
         this.lyd = lyd;
+    }
+    public String look(){
+        return "Du ser på et pink lysende sværd på jorden. Græsset omkring sværet er helt brændt ned.";
+    }
+    @Override
+    public String toString(){
+        return "Du er tæt på på" + this.getNavn() + "(" + this.getBeskrivelse() + "). Du laver et par slag ud i luften.." + this.getLyd() + ". Du kan mærke kraften fra sværdet gennemstrømme din krop.";
+    }
 
 
     }
@@ -27,10 +36,12 @@ public class FluffyBunny_sværd extends Genstand {
         FluffyBunny_sværd fluffybunny = new FluffyBunny_sværd();
         System.out.println("Sværd " + fluffybunny.getNavn() + "(" + fluffybunny.getBeskrivelse() + "). Du laver et par slag ud i luften.." + fluffybunny.getLyd() + ". Du kan mærke kraften fra sværdet gennemstrømme din krop.");
 
+        Kode til at køre sværdet. ToString virker på begge nu.
+        FluffyBunny_sværd fluffyBunny = new FluffyBunny_sværd();
+        System.out.println(fluffyBunny.toString());
 */
 
 
-}
 
 
 
