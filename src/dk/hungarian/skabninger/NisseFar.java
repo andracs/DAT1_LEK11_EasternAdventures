e9bfaf9d132c1441312788b4112a44f945ccb8eb
niel905g <31951130+niel905g@users.noreply.github.com>
2017-11-23 17:01:04 +0100

    Update NisseFar.java

--- src/dk/hungarian/skabninger/NisseFar.java
+++ src/dk/hungarian/skabninger/NisseFar.java
@@ -5,7 +5,7 @@ import dk.hungarian.Skabning;
 
 public class NisseFar extends Skabning{
     private int hvorArrig = 100;
-    int malechouvenist;
+    
 
 
     public NisseFar(String navn, String beskrivelse, int styrke, Gender gender) {
@@ -13,13 +13,13 @@ public class NisseFar extends Skabning{
     }
 
     public NisseFar(int hvorArrig) {
-        super("larsLykke", "Nissefar er meget farlig når han er 100% malechouvenistisk arrig!", 100, Gender.VATNISSE);
+        super("larsLykke", "Nissefar er meget farlig når han er 100% arrig!", 100, Gender.VATNISSE);
         this.hvorArrig = hvorArrig;
     }
 
     @Override
     public String toString(){
-        return " You have triggered " + this.getNavn() + " er " + malechouvenist;
+        return " Du er stødt på " + this.getNavn() + " og han er en" + hvor.Arrig + "%" + " arrig " + Gender + ", så pas på GroedSkeen!";
     }
 
     public int getHvorArrig() {
